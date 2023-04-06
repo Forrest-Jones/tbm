d06509deb4ff0eb2f4e3af41b4958c1adcd5ccd9
Forrest Jones <forrestjones2010@gmail.com>
2023-04-06 14:36:07 -0500

    Proverbs 18:21

--- client/my-react-app/src/App.js
+++ client/my-react-app/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Landing from './Landing.jsx';
 import SignUp from './SignUp.jsx';
+import SignUpPage from './SignUpPage.jsx';
+import Nav from './Nav.jsx';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (

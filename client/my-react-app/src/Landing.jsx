afd8c06da1b582972eeace1cdca186272e8bd705
Forrest Jones <forrestjones2010@gmail.com>
2023-04-06 10:52:47 -0500

    PR1821

---
+++ client/my-react-app/src/Landing.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Landing.css"
+
+function Landing({user, setUser}) {
+  return (
+    <div className="homepage">
+      <h1 className="title">The Blessing Machine</h1>
+      <p className="description">
+        Transforming Fundraising for Christian Organizations.
+        <br />
+        The Blessing Machine is a groundbreaking non-profit donation platform, tailored specifically for Christian organizations to raise capital for foreign and domestic missions work. It is aimed at revolutionizing the way they raise and manage funds through a dynamic platform that fosters growth and transparency. TBM accomplishes this by harnessing the power of advanced technologies like Artificial Intelligence(AI) and machine learning via partners like OpenAI through ChatGPT for admin and site coaching support, Google Brain, and DeepMind to name a few. The Blessing Machine is the premier AI supported platform that streamlines the fundraising process, offers a user-friendly, easy to navigate web experience for donors while insuring efficient, private, and secured, endowment management for the beneficiary organizations. TBM is poised to reshape the fundraising landscape for Christian organizations guaranteeing their long-term sustainability and growth as they do good work in the world.
+      </p>
+      <div className="cta">
+        <a href="/signup" className="button">Sign Up</a>
+      </div>
+      <div className="image">
+        <img src="example-image.jpg" alt="Example Image" />
+      </div>
+      </div>
+  );
+}
+
+export default Landing;

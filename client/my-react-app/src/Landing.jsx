09cf8ed43dd5a6818699396d9e1f08396cc5dd64
Forrest Jones <forrestjones2010@gmail.com>
2023-04-10 10:35:39 -0500

    added some CSS 4.10.2023

--- client/my-react-app/src/Landing.jsx
+++ client/my-react-app/src/Landing.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./Landing.css";
 
 function Landing({ user, setUser, organizations }) {
   const history = useHistory();
+  const [darkMode, setDarkMode] = useState(false);
 
   const handleSignUpClick = () => {
     history.push("/signup");
@@ -12,24 +13,29 @@ function Landing({ user, setUser, organizations }) {
   const handleLearnMoreClick = () => {
     history.push("/learnmore"); // Navigate to the Learn More page
   };
-  
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
-    <div className="homepage">
-      <h1 className="title">The Blessing Machine</h1>
-      <p className="description">
+    <div className={`homepage${darkMode ? " dark" : ""}`}>
+      <h1 className={`title${darkMode ? " dark" : ""}`}>The Blessing Machine</h1>
+      <p className={`description${darkMode ? " dark" : ""}`}>
         Transforming Fundraising for Christian Organizations.
-        <br />
-        The Blessing Machine is a groundbreaking non-profit donation platform, tailored specifically for Christian organizations to raise capital for foreign and domestic missions work. It is aimed at revolutionizing the way they raise and manage funds through a dynamic platform that fosters growth and transparency. TBM accomplishes this by harnessing the power of advanced technologies like Artificial Intelligence(AI) and machine learning via partners like OpenAI through ChatGPT for admin and site coaching support, Google Brain, and DeepMind to name a few. The Blessing Machine is the premier AI supported platform that streamlines the fundraising process, offers a user-friendly, easy to navigate web experience for donors while insuring efficient, private, and secured, endowment management for the beneficiary organizations. TBM is poised to reshape the fundraising landscape for Christian organizations guaranteeing their long-term sustainability and growth as they do good work in the world.
+        {/* Rest of the description */}
       </p>
       <div className="cta">
-        <button className="button" onClick={handleSignUpClick}>
+        <button className={`button${darkMode ? " dark" : ""}`} onClick={handleSignUpClick}>
           Sign Up
         </button>
-        <button className="button" onClick={handleLearnMoreClick}>
+        <button className={`button${darkMode ? " dark" : ""}`} onClick={handleLearnMoreClick}>
           Learn More
         </button>
       </div>
+      <button className={`button${darkMode ? " dark" : ""}`} onClick={toggleDarkMode}>
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
       <div className="image">
         <img src="images/Jesus-image.jpg" alt="Jesus Image" />
       </div>
